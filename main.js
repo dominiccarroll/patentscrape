fe169a5befcfaeb813b74356f4b3b59f29ec8c6c
dominiccarroll <dominic.carroll@email.com>
2022-11-16 15:45:50 -0500

    tryna scrape rss not workin tho

---
+++ main.js
@@ -0,0 +1,22 @@
+let Parser = require('rss-parser');
+let parser = new Parser({
+        timeout: 5000,
+    });
+
+(async () => {
+
+    let url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom";
+    let feed = await parser.parseURL(url);
+
+    return feed.title;
+
+})().then(
+    (value) => {
+        console.log("Returned: " + value);
+    },
+    (error) => {
+        if (error) {
+            console.log("error: " + error);
+        }
+    }
+)
