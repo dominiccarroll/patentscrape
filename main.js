227894ccf07b76cf7708ba511618474ecd3b7b7f
dominiccarroll <dominic.carroll@email.com>
2022-11-16 23:09:10 -0500

    iterate through recent 8k filings

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 let Parser = require('rss-parser');
 let parser = new Parser({
         timeout: 5000,
         headers: {'User-Agent': 'Dominic Carroll dom@dominiccarroll.com'}
     });
 
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 (async () => {
+    let output = '';
+    let filingsIndex = [0, 40]
+    
 
-    let url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom";
-    let feed = await parser.parseURL(url);
+    // iterate through feed pages
+    for (var i = 0; i < 20; i++) {
+        let url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=8-k&company=&dateb=&owner=include&start=" + filingsIndex[0] + "&count=" + filingsIndex[1] + "&output=atom";
+        let feed = await parser.parseURL(url);
 
-    return feed.title;
+        feed.items.forEach(item => {
+
+            let textUrl = item.link.replace('-index.htm', '.txt');
+            
+            console.log(textUrl);
+
+            
+        });
+
+
+        // New page
+        filingsIndex[0] = filingsIndex[0] + 41;
+        filingsIndex[1] = filingsIndex[1] + 40;
+
+
+    }
+    
+
+    return output;
 
 })().then(
     (value) => {
